5415e0d62e32b5cf7b92de2ca3fcfba074612a7e
KanathipP <miniexor@gmail.com>
2024-12-15 18:57:08 +0700

    category component design

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from "./cardList.module.css"
 import Pagination from '../pagination/Pagination'
+import Image from 'next/image'
+import Card from '../card/Card'
 
 const CardList = () => {
   return (
-    <div className={styles.container}>CardList
+    <div className={styles.container}>
+      <h1 className={styles.title}>Recents Posts</h1>
+      <div className={styles.posts}>
+        <div className={styles.post}>
+          <Card/>
+        </div>
+      </div>
     <Pagination/>
     </div>  
   )
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import styles from "./featured.module.css"
+import React from "react";
+import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, my name is LILbossX!</b> the real boss x hehe!
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Titleeeeeeeeeeeeee</h1>
+          <p className={styles.postDesc}>
+            descript ionnnn nnnnn nnnnn nnnn nnnnn nnnnn nnnn nnnnnn nnnnnn nnnnnn
+          </p>
+          <button className={styles.button}>Read more</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;
